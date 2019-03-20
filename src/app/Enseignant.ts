0cffe7362f4f1cd160b2689d7e69080b1ef0fbf0
59013-64-03 <59013-64-03@exchange.ad.afpanet>
2019-03-20 16:48:45 +0100

    init project

---
+++ src/app/Enseignant.ts
@@ -0,0 +1,12 @@
+export class Enseignant{
+    id: number
+    numero:number;
+    nom:string;
+    prenom: string;
+    dateNaissance:string;
+    dateEmbauche:string;
+    grade:string;
+    sexe:string;
+    photo:string;
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Enseignant } from './Enseignant';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  title = 'Hellooooooo!';
+  s:string = 'Hello';
+
+  numero:number = 125;
+
+  afficherLaListe: boolean = true;
+
+//enseignant
+nom: string = 'Jane';
+prenom:string = 'Jones';
+dateNaissance:string = '1990-01-01';
+dateEmbauche:string = '2019-02-02';
+sexe:string='F';
+grade:string = 'ASS';
+photo:string = 'https://cdn3.iconfinder.com/data/icons/pretty-office-part-10-shadow-style/512/Teacher-female.png';
+
+
+//enseignant objet
+enseignant:Enseignant = {
+  id:1,
+  nom :'Tom',
+  prenom:'Eric',
+  numero:124563,
+  dateEmbauche:'2008-01-02',
+  dateNaissance:'1988-04-08',
+  grade:'MCF',
+  sexe:'M',
+  photo:'https://d3iw72m71ie81c.cloudfront.net/gaurav.JPG'
+
+};
+
+enseignants:Enseignant[]=
+[
+  {
+    id:1,
+    nom :'Tom',
+    prenom:'Eric',
+    numero:124563,
+    dateEmbauche:'2008-01-02',
+    dateNaissance:'1988-04-08',
+    grade:'MCF',
+    sexe:'M',
+    photo:'https://d3iw72m71ie81c.cloudfront.net/gaurav.JPG'
+  
+  },
+  {
+    id:1,
+    nom :'Tom',
+    prenom:'Eric',
+    numero:124563,
+    dateEmbauche:'2008-01-02',
+    dateNaissance:'1988-04-08',
+    grade:'MCF',
+    sexe:'M',
+    photo:'https://d3iw72m71ie81c.cloudfront.net/gaurav.JPG'
+  
+  },
+  {
+    id:1,
+    nom :'Tom',
+    prenom:'Eric',
+    numero:124563,
+    dateEmbauche:'2008-01-02',
+    dateNaissance:'1988-04-08',
+    grade:'MCF',
+    sexe:'M',
+    photo:'https://d3iw72m71ie81c.cloudfront.net/gaurav.JPG'
+  
+  },
+  {
+    id:1,
+    nom :'Tom',
+    prenom:'Eric',
+    numero:124563,
+    dateEmbauche:'2008-01-02',
+    dateNaissance:'1988-04-08',
+    grade:'MCF',
+    sexe:'M',
+    photo:'https://d3iw72m71ie81c.cloudfront.net/gaurav.JPG'
+  
+  }
+];
+
 }
